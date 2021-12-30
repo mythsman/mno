2725e76106357d2a858097660971be854c48f52f
mythsman <mythsman@foxmail.com>
2021-12-30 14:37:18 +0800

    support paragraph_indent;modify first page;change 404

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,36 +1,5 @@
 $(document).ready(function() {
 
-  $('a.blog-button').click(function(evt) {
-    // If already in blog, return early without animate overlay panel again.
-    let targetIsBlog = evt.target.hash && evt.target.hash == "#blog";
-    let currentIsBlog = location.hash && location.hash == "#blog"
-    if ( targetIsBlog && currentIsBlog || !targetIsBlog && !currentIsBlog){
-      return;
-    }
-    if (!$('.panel-cover').hasClass('panel-cover--collapsed')) {
-      $('.panel-cover').addClass('panel-cover--collapsed')
-    }
-    $('.main-post-list').removeClass('hidden');
-    currentWidth = $('.panel-cover').width();
-    if (currentWidth < 960) {
-      $('.panel-cover').addClass('panel-cover--collapsed');
-      //$('.content-wrapper').addClass('animated slideInRight');
-    } else {
-      $('.panel-cover').css('max-width',currentWidth);
-      $('.panel-cover').animate({'max-width': '700px', 'width': '20%'}, 400, swing = 'swing', function() {} );
-    }
-  });
-
-  if (window.location.pathname == "/"  && window.location.hash != "#blog") {
-    $('.panel-cover').removeClass('panel-cover--collapsed');
-    $('.main-post-list').addClass('hidden');
-  }else{
-    if(!$('.panel-cover').hasClass('panel-cover--collapsed')){
-      $('.panel-cover').addClass('panel-cover--collapsed');
-    }
-    $('.main-post-list').removeClass('hidden');
-  }
-
   $('.btn-mobile-menu__icon').click(function() {
     $('.navigation-wrapper').toggleClass('visible');
     $('.btn-mobile-menu__icon').toggleClass('fa fa-list fa fa-angle-up animated fadeIn');
