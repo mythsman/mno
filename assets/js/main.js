9e6cfec8b477d2f88b5bd95b175d9b519414fd3a
mythsman <mythsman@foxmail.com>
2021-12-30 22:39:18 +0800

    enhance animate

--- assets/js/main.js
+++ assets/js/main.js
@@ -6,16 +6,43 @@ function ready(fn) {
   }
 }
 
+const animateCSS = (element, animation, prefix = 'animate__') =>
+  // We create a Promise and return it
+  new Promise((resolve, reject) => {
+    const animationName = `${prefix}${animation}`;
+    const node = document.querySelector(element);
+
+    node.classList.add(`${prefix}animated`, animationName);
+
+    // When the animation ends, we clean the classes and resolve the Promise
+    function handleAnimationEnd(event) {
+      event.stopPropagation();
+      node.classList.remove(`${prefix}animated`, animationName);
+      resolve('Animation ended');
+    }
+
+    node.addEventListener('animationend', handleAnimationEnd, {once: true});
+  });
+
 window.ready(function(){
   document.querySelector('.btn-mobile-menu').addEventListener('click',function(){
-    document.querySelector('.navigation-wrapper').classList.toggle('visible');
-    document.querySelector('.navigation-wrapper').classList.toggle('animated');
-    document.querySelector('.navigation-wrapper').classList.toggle('fadeIn');
+    if(document.querySelector('.navigation-wrapper').classList.contains('visible')){
+      animateCSS('.navigation-wrapper','fadeOutUp').then((msg)=>{
+        document.querySelector('.navigation-wrapper').classList.toggle('visible');
+      });
+      document.querySelector('.btn-mobile-menu__icon').classList.toggle('fa-angle-up');
+      document.querySelector('.btn-mobile-menu__icon').classList.toggle('fa-list');
+      animateCSS('.btn-mobile-menu__icon','fadeIn');
+    }else{
+      document.querySelector('.navigation-wrapper').classList.toggle('visible');
+      animateCSS('.navigation-wrapper','fadeInDown');
+      document.querySelector('.btn-mobile-menu__icon').classList.toggle('fa-angle-up');
+      document.querySelector('.btn-mobile-menu__icon').classList.toggle('fa-list');
+      animateCSS('.btn-mobile-menu__icon','fadeOut');
+    }
 
-    let menu = document.querySelector('.btn-mobile-menu__icon');
-    ['fa-list','fa-angle-up','animated','fadeIn'].forEach(function(className){
-      menu.classList.toggle(className);
-    })
+
+
   }, false);
 
 });
