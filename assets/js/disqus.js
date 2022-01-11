b8aa796c46eb101ed62b27fe7a98347ae46ec106
mythsman <mythsman@foxmail.com>
2022-01-11 19:10:33 +0800

    fix disqus

---
+++ assets/js/disqus.js
@@ -0,0 +1,53 @@
+function disqus_init(config) {
+    let link = config.link;
+    let btn = config.btn;
+
+    document.querySelector(btn).addEventListener('click', () =>{
+        document.querySelector(btn).classList.add('hidden');
+        show_disqus();
+    });
+
+    function hide_ads() {
+        setInterval(() =>{
+            let iframes = document.getElementsByTagName('iframe');
+	    for (let idx in iframes) {
+                let iframe = iframes[idx];
+                if (iframe.title === 'Disqus' && !iframe.src) {
+                    iframe.parentElement.removeChild(iframe)
+                }
+            }
+        },
+        1000)
+    }
+
+    function show_disqus_btn() {
+        document.querySelector(btn).classList.remove('hidden');
+    }
+
+    function show_disqus() {
+        console.log("Facebook contected , disqus is shown.");
+        var d = document,
+        s = d.createElement('script');
+        s.defer = "defer";
+	s.src = link;
+	s.setAttribute('data-timestamp', +new Date()); 
+	(d.head || d.body).appendChild(s);
+        hide_ads();
+    }
+
+    function hide_disqus_btn() {
+        console.log("GFW detected , disqus is hidden.");
+    }
+
+    fetchRequest("https://graph.facebook.com/feed?callback=h", {},
+    1000).then(res =>{
+        if (res.status === 200) {
+            show_disqus_btn();
+        } else {
+            hide_disqus_btn();
+        }
+    }).
+    catch(e =>{
+        hide_disqus_btn();
+    })
+}
